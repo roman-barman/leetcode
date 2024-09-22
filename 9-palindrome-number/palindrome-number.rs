dcea43d542cf23244e8c9728c21cf08b0464cc5a
Raman Zanko <roman.zanko.vl@gmail.com>
2024-09-22 17:19:48 +0400

    Time: 3 ms (78.17%) | Memory: 2.1 MB (27.71%) - LeetSync

--- 9-palindrome-number/palindrome-number.rs
+++ 9-palindrome-number/palindrome-number.rs
@@ -6,28 +6,14 @@ impl Solution {
             return false;
         }
 
-        let mut div = x;
-        let mut calc = Vec::new();
+        let mut n = x;
+        let mut reversed = 0;
 
-        while div > 9 {
-            calc.push(div % 10);
-            div = div / 10;
-        }
-        calc.push(div);
-
-        let mut index = 0;
-        let mut last_index = calc.len() - 1;
-        let middle = calc.len() / 2;
-
-        while index <= middle  {
-            if calc.index(index) != calc.index(last_index) {
-                return false;
-            }
-
-            index = index + 1;
-            last_index = last_index - 1;
+        while n != 0 {
+            reversed = reversed * 10 + n % 10;
+            n /= 10;
         }
 
-        true
+        x == reversed
     }
 }
