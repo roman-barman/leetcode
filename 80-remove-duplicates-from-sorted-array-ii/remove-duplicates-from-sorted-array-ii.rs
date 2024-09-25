dc8a10e5cd4973d64c605bfe894bb3fcc8019d57
Raman Zanko <roman.zanko.vl@gmail.com>
2024-09-25 17:31:07 +0400

    Time: 1 ms (77.54%) | Memory: 2.1 MB (51.93%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.rs
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.rs
@@ -1,19 +1,11 @@
 impl Solution {
     pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
         let mut result = 1;
-        let mut dublicate_count = 0;
 
         for index in 1..nums.len() {
-            if nums[index] != nums[index - 1] {
+            if result == 1 || nums[index] != nums[result - 2] {
                 nums[result] = nums[index];
-                dublicate_count = 0;
                 result = result + 1;
-            } else if dublicate_count < 1 {
-                nums[result] = nums[index];
-                dublicate_count = dublicate_count + 1;
-                result = result + 1;
-            } else {
-                dublicate_count = dublicate_count + 1;
             }
         }
 
