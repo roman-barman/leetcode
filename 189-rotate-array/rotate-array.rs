7b55d0043be960980cccccb388a20242721ee863
Raman Zanko <roman.zanko.vl@gmail.com>
2024-09-26 16:49:53 +0400

    Time: 368 ms (5.63%) | Memory: 3.6 MB (77.82%) - LeetSync

---
+++ 189-rotate-array/rotate-array.rs
@@ -0,0 +1,27 @@
+impl Solution {
+    pub fn rotate(nums: &mut Vec<i32>, k: i32) {
+        if nums.len() < 2 {
+            return;
+        }
+
+        let last_index = nums.len() - 1;
+
+        for _ in 0..k {
+            let temp = nums[last_index];
+
+            let mut j = last_index - 1;
+
+            loop {
+                nums[j + 1] = nums[j];
+
+                if j == 0 {
+                    break;
+                }
+
+                j = j - 1;
+            }
+
+            nums[0] = temp;
+        }
+    }
+}
