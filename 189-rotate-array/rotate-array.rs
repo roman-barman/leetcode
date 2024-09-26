8f8f03458aaa0c96dd8c20054b5b360fe929de26
Raman Zanko <roman.zanko.vl@gmail.com>
2024-09-26 19:51:03 +0400

    Time: 350 ms (6.87%) | Memory: 3.5 MB (77.82%) - LeetSync

--- 189-rotate-array/rotate-array.rs
+++ 189-rotate-array/rotate-array.rs
@@ -6,7 +6,7 @@ impl Solution {
 
         let last_index = nums.len() - 1;
 
-        for _ in 0..k {
+        for _ in 0..(k as usize % nums.len()) {
             let temp = nums[last_index];
 
             let mut j = last_index - 1;
