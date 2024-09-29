142324e52a511c4a7563922ccb3be1fc2218d724
Raman Zanko <roman.zanko.vl@gmail.com>
2024-09-29 14:30:41 +0400

    Time: 34 ms (85.58%) | Memory: 4.6 MB (27.88%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.rs
+++ 739-daily-temperatures/daily-temperatures.rs
@@ -3,14 +3,17 @@ impl Solution {
         let len = temperatures.len();
         let mut result = vec![0i32; len];
         let mut stack = Vec::with_capacity(len);
+        let mut last_index = - 1;
 
         for i in 0..len {
-            while stack.len() > 0 &&  temperatures[*(stack.last().unwrap())] < temperatures[i] {
+            while stack.len() > 0 &&  temperatures[stack[last_index as usize]] < temperatures[i] {
                 let index = stack.pop().unwrap();
+                last_index = last_index - 1;
                 result[index] = (i - index) as i32;
             }
 
             stack.push(i);
+            last_index = last_index + 1;
         }
 
         result
