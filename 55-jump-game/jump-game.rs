ecf095ab5605c5f62b793a9f7eb0e326dc12d795
Raman Zanko <roman.zanko.vl@gmail.com>
2024-10-03 10:03:21 +0400

    Time: 32 ms (24.41%) | Memory: 2.2 MB (51.97%) - LeetSync

--- 55-jump-game/jump-game.rs
+++ 55-jump-game/jump-game.rs
@@ -5,28 +5,23 @@ impl Solution {
         }
 
         let last_index = nums.len() - 1;
-
-        let mut jumps = Vec::with_capacity(nums.len());
         let mut is_visited = vec![false;nums.len()];
-        jumps.push(0);
+        let mut max_index = 0;
 
-        while !jumps.is_empty() {
-            let current_index = jumps.pop().unwrap();
-            is_visited[current_index] = true;
-            let max_index = current_index + nums[current_index] as usize;
-            if max_index >= last_index {
-                return true
+        while max_index < last_index {
+            if is_visited[max_index] && max_index == 0 {
+                return false;
+            }
+            if is_visited[max_index] {
+                max_index = max_index - 1;
+                continue;
+            } else {
+                is_visited[max_index] = true;
             }
 
-            if max_index > current_index {
-                for i in current_index + 1..=max_index {
-                    if !is_visited[i] {
-                        jumps.push(i);
-                    }
-                }
-            }
+            max_index = max_index + nums[max_index] as usize;
         }
 
-        false
+        true
     }
 }
