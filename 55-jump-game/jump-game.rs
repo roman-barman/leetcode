a01a3dc2b80872353a0f94c4187254fc710c8d74
Raman Zanko <roman.zanko.vl@gmail.com>
2024-10-03 10:13:35 +0400

    Time: 6 ms (75.07%) | Memory: 2.2 MB (95.54%) - LeetSync

--- 55-jump-game/jump-game.rs
+++ 55-jump-game/jump-game.rs
@@ -1,27 +1,15 @@
 impl Solution {
     pub fn can_jump(nums: Vec<i32>) -> bool {
-        if nums.len() == 0 {
-            return false;
-        }
+        let len = nums.len();
 
-        let last_index = nums.len() - 1;
-        let mut is_visited = vec![false;nums.len()];
-        let mut max_index = 0;
+        let mut furthest_reachable = 0;
 
-        while max_index < last_index {
-            if is_visited[max_index] && max_index == 0 {
+        for i in 0..len-1 {
+            furthest_reachable = furthest_reachable.max(i + nums[i] as usize);
+            if furthest_reachable == i {
                 return false;
             }
-            if is_visited[max_index] {
-                max_index = max_index - 1;
-                continue;
-            } else {
-                is_visited[max_index] = true;
-            }
-
-            max_index = max_index + nums[max_index] as usize;
         }
-
         true
     }
 }
