67d7c14e87a551218cc165e49f6d18c363ab88f5
Raman Zanko <roman.zanko.vl@gmail.com>
2024-10-10 08:16:29 +0400

    Time: 37 ms (22.37%) | Memory: 2.2 MB (69.74%) - LeetSync

--- 18-4sum/4sum.rs
+++ 18-4sum/4sum.rs
@@ -8,24 +8,25 @@ impl Solution {
             return Vec::new();
         }
 
-        let mut nums = nums.clone();
-        nums.sort();
+        let target = target as i64;
+        let mut nums : Vec<i64> = nums.into_iter().map(|x| x as i64).collect();
+        nums.sort_unstable();
 
-        let mut set : HashSet<Vec<i32>> = HashSet::new();
+        let mut set : HashSet<Vec<i32>> = HashSet::with_capacity(256);
 
         for i in 0..len - 3 {
             for j in i + 1..len - 2 {
-                let new_target = target as i64 - nums[i] as i64 - nums[j] as i64;
+                let new_target = target - nums[i] - nums[j];
                 let mut low = j + 1;
                 let mut high = len -1;
 
                 while low < high {
-                    if (nums[low] as i64) + (nums[high] as i64) < new_target {
+                    if (nums[low]) + (nums[high]) < new_target {
                         low = low + 1;
-                    } else if (nums[low] as i64) + (nums[high] as i64) > new_target {
+                    } else if (nums[low]) + (nums[high]) > new_target {
                         high = high - 1;
                     } else {
-                        set.insert(vec![nums[i],nums[j],nums[low],nums[high]]);
+                        set.insert(vec![nums[i] as i32,nums[j] as i32,nums[low] as i32,nums[high] as i32]);
                         low = low + 1;
                         high = high - 1;
                     }
