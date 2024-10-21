ba02374fd387985df02414fa47cb362b3bc296cc
Raman Zanko <roman.zanko.vl@gmail.com>
2024-10-21 17:13:39 +0400

    Time: 94 ms (56.52%) | Memory: 11.3 MB (13.04%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.rs
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.rs
@@ -1,15 +1,64 @@
 impl Solution {
     pub fn count_smaller(nums: Vec<i32>) -> Vec<i32> {
-        let mut nums_sorted = vec![];
-        let mut result = vec![];
-        nums.iter().rev().for_each(|&n| {
-            let pos = nums_sorted
-                .binary_search_by(|probe: &i32| probe.cmp(&(2 * n - 1)))
-                .unwrap_or_else(|x| x);
-            result.push(pos as i32);
-            nums_sorted.insert(pos, 2 * n);
-        });
-        result.reverse();
+        let mut ans = vec![0; nums.len()];
+        let mut nums = nums.into_iter().enumerate().collect();
+        Self::merge_sort(&mut nums, &mut ans);
+        ans
+    }
+
+    pub fn merge_sort(nums: &Vec<(usize, i32)>, ans: &mut Vec<i32>) -> Vec<(usize, i32)> {
+        if nums.len() < 2 {
+            return nums.clone();
+        }
+
+        let mut left = Vec::new();
+        let mut right = Vec::new();
+
+        let middle = nums.len() / 2;
+
+        for i in 0..middle {
+            left.push(nums[i]);
+        }
+
+        for i in middle..nums.len() {
+            right.push(nums[i]);
+        }
+
+        let sorted_left = Self::merge_sort(&left, ans);
+        let sorted_right = Self::merge_sort(&right, ans);
+
+        Self::merge(&sorted_left, &sorted_right, ans)
+    }
+
+    pub fn merge(left: &Vec<(usize, i32)>, right: &Vec<(usize, i32)>, ans: &mut Vec<i32>) -> Vec<(usize, i32)> {
+        let mut result = Vec::with_capacity(left.len() + right.len());
+        let mut i = 0;
+        let mut j = 0;
+        let mut count = 0;
+
+        while i < left.len() && j < right.len() {
+            if left[i].1 > right[j].1 {
+                result.push(right[j]);
+                j += 1;
+                count += 1;
+            } else {
+                result.push(left[i]);
+                ans[left[i].0] += count;
+                i += 1;
+            }
+        }
+
+        while i < left.len() {
+            ans[left[i].0] += right.len() as i32;
+            result.push(left[i]);
+            i += 1;
+        }
+
+        while j < right.len() {
+            result.push(right[j]);
+            j += 1;
+        }
+
         result
     }
 }
