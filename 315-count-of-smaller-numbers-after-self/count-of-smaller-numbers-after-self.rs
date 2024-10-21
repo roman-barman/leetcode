b07a01666b642f9265f351f742df2f942c0b4f5f
Raman Zanko <roman.zanko.vl@gmail.com>
2024-10-21 08:52:07 +0400

    Time: 757 ms (47.83%) | Memory: 4.2 MB (30.43%) - LeetSync

--- 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.rs
+++ 315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.rs
@@ -1,18 +1,15 @@
 impl Solution {
     pub fn count_smaller(nums: Vec<i32>) -> Vec<i32> {
-        let mut result = vec![0;nums.len()];
-
-        for i in 0..nums.len() {
-            let mut count = 0;
-            for j in i + 1 ..nums.len() {
-                if nums[i] > nums[j] {
-                    count = count + 1;
-                }
-            }
-
-            result[i] = count;
-        }
-
+        let mut nums_sorted = vec![];
+        let mut result = vec![];
+        nums.iter().rev().for_each(|&n| {
+            let pos = nums_sorted
+                .binary_search_by(|probe: &i32| probe.cmp(&(2 * n - 1)))
+                .unwrap_or_else(|x| x);
+            result.push(pos as i32);
+            nums_sorted.insert(pos, 2 * n);
+        });
+        result.reverse();
         result
     }
 }
