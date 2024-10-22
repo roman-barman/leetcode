bc1d05522d2c70372d18282b5f7e6a0e9fce1f17
Raman Zanko <roman.zanko.vl@gmail.com>
2024-10-22 08:12:06 +0400

    Time: 3 ms (56.25%) | Memory: 2.8 MB (68.75%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.rs
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.rs
@@ -20,22 +20,26 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn build_tree(preorder: Vec<i32>, inorder: Vec<i32>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::build_tree_from_slice(&preorder.as_slice(), &inorder.as_slice())
+    }
+
+    pub fn build_tree_from_slice(preorder: &[i32], inorder: &[i32]) -> Option<Rc<RefCell<TreeNode>>>{
         let mut root = TreeNode::new(preorder[0]);
 
-        let i = inorder.iter().position(|&x| x == preorder[0])?;
-        let inorder_left = inorder[0..i].to_vec();
-        let inorder_right = inorder[i+1..].to_vec();
-        
-        let preorder_left = preorder[1..1+inorder_left.len()].to_vec();
-        let preorder_right = preorder[1+inorder_left.len()..].to_vec();
+        let index = inorder.iter().position(|&x| x == preorder[0])?;
+        let inorder_left = &inorder[..index];
+        let inorder_right = &inorder[(index + 1)..];
+
+        let preorder_left = &preorder[1..(1 + index)];
+        let preorder_right = &preorder[(1 + index)..];
 
         if inorder_left.len() > 0 {
-            root.left = Self::build_tree(preorder_left, inorder_left);
+            root.left = Self::build_tree_from_slice(&preorder_left, &inorder_left);
         }
         if inorder_right.len() > 0 {
-            root.right = Self::build_tree(preorder_right, inorder_right);
+            root.right = Self::build_tree_from_slice(&preorder_right, &inorder_right);
         }
-        
+
         Some(Rc::new(RefCell::new(root)))
     }
 }
