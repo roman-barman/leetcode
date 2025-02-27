2d166c15636f0d0c05e09e1bec7f63c791b2a8f3
Raman Zanko <roman.zanko.vl@gmail.com>
2025-02-27 15:59:55 +0400

    Time: 0 ms (100.00%) | Memory: 2.5 MB (14.49%) - LeetSync

--- 443-string-compression/string-compression.rs
+++ 443-string-compression/string-compression.rs
@@ -6,7 +6,7 @@ impl Solution {
         
         let mut count = 1;
         let mut result = 0;
-        let mut last_index = 0 as usize;
+        let mut last_index = 0usize;
         
         for i in 0..chars.len() - 1 {
             if chars[i] == chars[i + 1] {
@@ -15,14 +15,8 @@ impl Solution {
                 chars[last_index] = chars[i];
                 last_index += 1;
                 result += 1;
-                if count >= 2 {                
-                    let count_chars = count.to_string().chars().collect::<Vec<char>>();
-                    
-                    for char in count_chars.iter() {
-                        chars[last_index] = *char;
-                        last_index += 1;
-                    }                
-                    result += count_chars.len() as i32;
+                if count >= 2 {
+                    result += Self::add_count(count, chars, &mut last_index);
                 }
                 count = 1;
             }
@@ -33,16 +27,36 @@ impl Solution {
         result += 1;
         
         if count >= 2 {
-            let count_chars = count.to_string().chars().collect::<Vec<char>>();
-
-            for char in count_chars.iter() {
-                chars[last_index] = *char;
-                last_index += 1;
-            }
-            result += count_chars.len() as i32;
+            result += Self::add_count(count, chars, &mut last_index);
         }
         
         chars.truncate(last_index);
+        
+        result
+    }
+
+    fn add_count(count: i32, chars: &mut Vec<char>, start_index: &mut usize) -> i32 {
+        let mut divider = 10;
+        let mut count = count;
+
+        while divider <= count {
+            divider *= 10;
+        }
+        
+        let mut result = 0;
+        
+        while divider > 10 {
+            divider /= 10;
+            let digit = count / divider;
+            chars[*start_index] = char::from_digit(digit as u32, 10).unwrap();
+            *start_index += 1;
+            count = count % divider;
+            result += 1;
+        }
+
+        chars[*start_index] = char::from_digit(count as u32, 10).unwrap();
+        *start_index += 1;
+        result += 1;
         
         result
     }
