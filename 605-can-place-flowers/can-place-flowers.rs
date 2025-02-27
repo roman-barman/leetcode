a4b42d236376a88caf3b148c801df4f2bc12933e
Raman Zanko <roman.zanko.vl@gmail.com>
2025-02-27 10:34:32 +0400

    Time: 0 ms (100.00%) | Memory: 2.5 MB (40.77%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.rs
@@ -0,0 +1,29 @@
+impl Solution {
+    pub fn can_place_flowers(flowerbed: Vec<i32>, n: i32) -> bool {
+        if n == 0 {
+            return true;
+        }
+
+        let mut flowerbed = flowerbed.clone();
+        let mut n = n;
+        
+        for i in 0..flowerbed.len() {
+            if flowerbed[i] == 1 {
+                continue;
+            }
+            
+            let previous = if i == 0 { 0 } else { flowerbed[i - 1] };
+            let next = if i == flowerbed.len() - 1 { 0 } else { flowerbed[i + 1] };
+            
+            if previous == 0 && next == 0 {
+                n = n - 1;
+                flowerbed[i] = 1;
+                if n == 0 {
+                    return true
+                }
+            }        
+        }    
+        
+        false
+    }
+}
