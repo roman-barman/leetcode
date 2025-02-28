da25396fee1b6658fcf7d3affb38a1300270318c
Raman Zanko <roman.zanko.vl@gmail.com>
2025-02-28 14:34:41 +0400

    Time: 0 ms (100.00%) | Memory: 3.2 MB (16.52%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.rs
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.rs
@@ -1,22 +1,13 @@
 impl Solution {
     pub fn find_max_average(nums: Vec<i32>, k: i32) -> f64 {
-         if nums.len() == 1 {
-            return nums[0] as f64;
+         let mut windows_sum = nums.iter().take(k as usize).sum::<i32>();
+        let mut max_sum = windows_sum;
+
+        for i in k as usize..nums.len() {
+            windows_sum += nums[i] - nums[i - k as usize];
+            max_sum = if max_sum > windows_sum { max_sum } else { windows_sum };
         }
-        
-        let mut max_sum = i32::MIN;
-        
-        for i in 0..nums.len() - (k - 1)  as usize {
-            let mut sum = 0;
-            for j in i..i + k as usize {
-                sum += nums[j];
-            }
-            
-            if sum > max_sum {
-                max_sum = sum;
-            }
-        }
-        
+
         max_sum as f64 / k as f64
     }
 }
