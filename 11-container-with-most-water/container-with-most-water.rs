6caba6f889dd4aecf28c344fdb95d424c5b3fab0
Raman Zanko <roman.zanko.vl@gmail.com>
2025-02-28 12:17:54 +0400

    Time: 0 ms (100.00%) | Memory: 3.1 MB (34.24%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.rs
+++ 11-container-with-most-water/container-with-most-water.rs
@@ -1,5 +1,3 @@
-use std::cmp::min;
-
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
         let mut left_index = 0usize;
@@ -8,7 +6,11 @@ impl Solution {
         let mut max_area = 0;
         
         while left_index <= right_index {
-            let min_height = min(height[left_index], height[right_index]);
+            let min_height = if height[left_index] > height[right_index] {
+                height[right_index]
+            } else { 
+                height[left_index]
+            };
             let length = (right_index - left_index) as i32;
             
             let area = min_height * length;
