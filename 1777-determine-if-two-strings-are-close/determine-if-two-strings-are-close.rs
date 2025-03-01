906d0e82a450c954233c367a72abc1ffadd9d11c
Raman Zanko <roman.zanko.vl@gmail.com>
2025-03-01 22:33:57 +0400

    Time: 0 ms (100.00%) | Memory: 2.5 MB (44.74%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.rs
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.rs
@@ -2,52 +2,19 @@ use std::collections::{HashMap, HashSet};
 
 impl Solution {
     pub fn close_strings(word1: String, word2: String) -> bool {
-        if word1.len() != word2.len() {
-            return false;
-        }
-
-        let set_1: HashSet<char> = HashSet::from_iter(word1.chars());
-        let set_2: HashSet<char> = HashSet::from_iter(word2.chars());    
-        if set_1.difference(&set_2).count() != 0 {
-            return false;
-        }
+        let (mut array_1, mut array_2) = ([0; 26], [0; 26]);
+        for c in word1.chars() { array_1[c as usize - 0x61] += 1; };
+        for c in word2.chars() { array_2[c as usize - 0x61] += 1; };
         
-        let map_1 = Self::create_count_map(word1);
-        let map_2 = Self::create_count_map(word2);    
-        for (key, value) in map_1 {
-            if !map_2.contains_key(&key) {
+        for i in 0..26 {
+            if (array_1[i] == 0 && array_2[i] != 0) || (array_1[i] != 0 && array_2[i] == 0) {
                 return false;
             }
-            
-            let length_1 =value.len();
-            let length_2 =map_2.get(&key).unwrap().len();
-            if length_1 != length_2 {
-                return false;
-            }
-        }   
-        
-        true
-    }
-
-    fn create_count_map(word1: String) -> HashMap<usize, Vec<char>> {
-        let mut chars_map: HashMap<char, usize> = HashMap::new();
-        for c in word1.chars() {
-            if chars_map.contains_key(&c) {
-                chars_map.insert(c, chars_map[&c] + 1);
-            } else {
-                chars_map.insert(c, 1);
-            }
-        }
-
-        let mut count_map: HashMap<usize, Vec<char>>  = HashMap::new();
-        for (key, value) in chars_map {
-            if count_map.contains_key(&value) {
-                count_map.get_mut(&value).unwrap().push(key);
-            } else {
-                count_map.insert(value, vec![key]);
-            }
         }
         
-        count_map
+        array_1.sort();
+        array_2.sort();
+
+        array_1 == array_2
     }
 }
