1a2430c1e6926257dae25643f88ed9bf566b2d92
Raman Zanko <roman.zanko.vl@gmail.com>
2025-03-02 10:50:46 +0400

    Time: 6 ms (15.76%) | Memory: 2.5 MB (81.28%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.rs
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.rs
@@ -1,11 +1,11 @@
 impl Solution {
     pub fn remove_stars(s: String) -> String {
-        let mut result = String::new();
-        for char in s.chars() {
-            if char == '*' {
-                result.pop();
-            } else { result.push(char) }
-        }
-        result
+        s.chars().fold(String::new(), |mut result, c| { 
+            match c {
+                '*' => { result.pop(); },
+                _ => result.push(c),
+            }; 
+            result
+        })
     }
 }
